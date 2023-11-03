2d238a0126e257b10cc4b45f03444d53de86e3ad
joao <joao.pedro@konatussolutions.com>
2023-11-03 11:48:53 -0300

    +

--- src/App.js
+++ src/App.js
@@ -1,73 +1,77 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Graph } from 'react-d3-graph';
-import * as d3 from 'd3';
+import ForceGraph2D from 'react-force-graph-2d';
 
 function ImageAnalysis() {
+  const [classes, setClasses] = useState([]);
   const [data, setData] = useState({ nodes: [], links: [] });
-  const graphContainer = useRef(null);
+  const [images, setImages] = useState({});
+  const [imageSize, setImageSize] = useState(0);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const path = 'C:\\Users\\aslir\\Documents\\Faculdade\\TCC\\front\\image-analysis\\public\\images';
-        const result = await axios(`http://127.0.0.1:8000/analyze_images/?folder_path=${path}`);
+  const fetchClasses = async () => {
+    try {
+      const result = await axios('http://127.0.0.1:8000/get_distinct_classes/');
+      if (result.data && Array.isArray(result.data)) {
+        setClasses(result.data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-        const nodes = result.data.nodes.map(node => ({
+  const fetchImagesByClass = async (class_label) => {
+    try {
+      const result = await axios(`http://127.0.0.1:8000/get_images_by_class/?class_label=${class_label}`);
+      if (result.data && Array.isArray(result.data)) {
+        const nodes = result.data.map((node) => ({
           id: node.id,
-          symbolType: 'image',
-          url: `/images/${node.id}`,
-          size: 400,
+          url: `/images/${node.id}`, // Modify this with actual path of images
         }));
-        const links = result.data.edges.map(edge => ({
-          source: edge.source,
-          target: edge.target,
-          color: 'black',
+        const links = nodes.map((node, i) => ({
+          source: node.id,
+          target: nodes[(i + 1) % nodes.length].id,
         }));
 
         setData({ nodes, links });
-      } catch (error) {
-        console.error(error);
+        setImageSize(Math.min(window.innerWidth, window.innerHeight) / (2 * Math.sqrt(nodes.length)));
       }
-    };
-
-    fetchData();
-  }, []);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
-    if (graphContainer.current) {
-      d3.select(graphContainer.current)
-          .select('#graph-id')
-          .attr('pointer-events', 'all')
-          .call(d3.zoom().scaleExtent([0.1, 2]));
-    }
-  }, [graphContainer]);
-
-  const myConfig = {
-    nodeHighlightBehavior: true,
-    node: {
-      color: 'lightgray',
-      size: 800,
-      highlightStrokeColor: 'blue',
-      renderLabel: false,
-      viewGenerator: node => <img src={node.url} alt={node.id} style={{ height: "100px", width: "100px" }}/>
-    },
-    link: {
-      highlightColor: 'lightblue',
-    },
-    d3: {
-      gravity: -400,
-    },
-    width: window.innerWidth * 0.8,
-    height: window.innerHeight - 200
-  };
+    data.nodes.forEach((node) => {
+      if (!images[node.id]) {
+        const img = new Image();
+        img.src = node.url;
+        img.onload = () => setImages((images) => ({ ...images, [node.id]: img }));
+      }
+    });
+  }, [data]);
 
   return (
       <div className="image-analysis">
         <h1 className="title">Análise de imagens</h1>
-        <div ref={graphContainer}>
-          <Graph id="graph-id" data={data} config={myConfig} />
-        </div>
+        <button onClick={fetchClasses}>Buscar classes</button>
+        {classes.length > 0 && (
+            <div>
+              {classes.map((item, index) => (
+                  <button key={index} onClick={() => fetchImagesByClass(item)}>{item}</button>
+              ))}
+            </div>
+        )}
+        <ForceGraph2D
+            graphData={data}
+            linkDistance={300} // Aumente para ocupar mais espaço
+            nodeCanvasObject={(node, ctx, globalScale) => {
+              const img = images[node.id];
+              if (img) {
+                const size = imageSize / globalScale;
+                ctx.drawImage(img, node.x - size / 2, node.y - size / 2, size, size);
+              }
+            }}
+        />
       </div>
   );
 }
